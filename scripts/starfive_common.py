1bd948f902198b626bacf8356d11848f4e1de483
rmsyn <rmsynchls@gmail.com>
2023-10-20 06:27:49 +0000

    fixup: scripts: add default `resetValue`
    Adds a default `resetValue` field to registers defined with
    `starfive_common/generate_register`.
    
    Allows SVD parsers to supply a default reset value for the register.
    
    Defaults to zero, which should be a safe reset value for most registers.
    Users that require a non-zero default can supply it as an argument to
    `generate_register`.

--- scripts/starfive_common.py
+++ scripts/starfive_common.py
@@ -157,13 +157,14 @@ def generate_registers_aon_rst_sel(name, desc, addr):
                 </register>
 """
 
-def generate_register(name, desc, addr, field_name_desc_range_access, size=32):
+def generate_register(name, desc, addr, field_name_desc_range_access, size=32, reset_value=0):
     txt = """\
                 <register>
                   <name>""" + name + """</name>
                   <description>""" + desc + """</description>
                   <addressOffset>""" + "0x{:x}".format(addr) + """</addressOffset>
                   <size>""" + str(size) + """</size>
+                  <resetValue>""" + str(reset_value) + """</resetValue>
                   <fields>
 """
 
