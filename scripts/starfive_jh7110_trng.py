e01c0cb49b559b4bcd32e585c94f77ed61a1caa4
rmsyn <rmsynchls@gmail.com>
2023-12-09 22:59:22 +0000

    fixup: trng: use `generate_register` help function
    Uses the `generate_register` helper function to increase code re-use,
    and more uniformity in register definitions.

--- scripts/starfive_jh7110_trng.py
+++ scripts/starfive_jh7110_trng.py
@@ -2,6 +2,8 @@
 # Copyleft (c) 2023 cmsis-svd-generator developers
 # SPDX-License-Identifier: Apache-2.0
 
+from scripts.starfive_common import *
+
 """
 This program generates CMSIS SVD xml for starfive JH7110 trng
 """
@@ -20,8 +22,8 @@ def generate_registers_starfive_jh7110_trng(dts, peripheral):
     txt += generate_register_trng_ie("ie");
     txt += generate_register_trng_istat("istat");
 
-    for idx in range(0, 8):
-        txt += generate_register_trng_rand("rand{}".format(idx), 0x20 + (idx * 4), idx)
+    for idx in range(8):
+        txt += generate_register_trng_rand("rand_{}".format(idx), 0x20 + (idx * 4), idx)
 
     txt += generate_register_trng_auto_rqsts("auto_rqsts")
     txt += generate_register_trng_auto_age("auto_age")
@@ -33,294 +35,91 @@ def generate_registers_starfive_jh7110_trng(dts, peripheral):
 
 def generate_register_trng_ctrl(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>TRNG CTRL Register</description>
-                  <addressOffset>0x0</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>exec_nop</name>
-                      <description>Execute a NOP instruction</description>
-                      <bitRange>[0:0]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>gene_randnum</name>
-                      <description>Generate a random number</description>
-                      <bitRange>[1:1]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>exec_randreseed</name>
-                      <description>Reseed the TRNG from noise sources</description>
-                      <bitRange>[2:2]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
-
+    return generate_register(name, "TRNG CTRL Register", 0x0, [
+        ("exec_nop", "Execute a NOP instruction", "[0:0]", "read-write"),
+        ("gen_rand", "Generate a random number", "[1:1]", "read-write"),
+        ("reseed", "Reseed the TRNG from noise sources", "[2:2]", "read-write")
+    ])
 
 def generate_register_trng_stat(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    txt = """\
-                <register>
-                  <name>stat</name>
-                  <description>TRNG STAT Register</description>
-                  <addressOffset>0x4</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>nonce_mode</name>
-                      <description>TRNG Nonce operating mode</description>
-                      <bitRange>[2:2]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>r256</name>
-                      <description>TRNG 256-bit random number operating mode</description>
-                      <bitRange>[3:3]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>mission_mode</name>
-                      <description>TRNG Mission Mode operating mode</description>
-                      <bitRange>[8:8]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>seeded</name>
-                      <description>TRNG Seeded operating mode</description>
-                      <bitRange>[9:9]</bitRange>
-                      <access>read-only</access>
-                    </field>
-"""
+    fields = [
+        ("nonce_mode", "TRNG Nonce operating mode", "[2:2]", "read-only"),
+        ("r256", "TRNG 256-bit random number operating mode", "[3:3]", "read-only"),
+        ("mission_mode", "TRNG Mission Mode operating mode", "[8:8]", "read-only"),
+        ("seeded", "TRNG Seeded operating mode", "[9:9]", "read-only")
+    ]
 
     # There are 8 RAND fields
     # FIXME: do these LAST_RESEED fields directly correspond to the RAND fields?
     # These fields are a best-guess based on the Linux char driver
-    for idx in range(0, 8):
+    for idx in range(8):
         # last reseed fields start at bit offset 16 (0x10 hex)
-        txt += generate_field_last_reseed(0x10 + idx, idx)
+        bit = 0x10 + idx
+        fields.append((
+            "last_reseed_{}".format(idx),
+            "TRNG Last Reseed {} status".format(idx),
+            "[{}:{}]".format(bit, bit),
+            "read-only"
+        ))
 
-    txt += """\
-                    <field>
-                      <name>srvc_rqst</name>
-                      <description>TRNG Service Request</description>
-                      <bitRange>[27:27]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>rand_generating</name>
-                      <description>TRNG Random Number Generating Status</description>
-                      <bitRange>[30:30]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>rand_seeding</name>
-                      <description>TRNG Random Number Seeding Status</description>
-                      <bitRange>[31:31]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    fields.extend([
+        ("srvc_rqst", "TRNG Service Request", "[27:27]", "read-only"),
+        ("rand_generating", "TRNG Random Number Generating Status", "[30:30]", "read-only"),
+        ("rand_seeding", "TRNG Random Number Seeding Status", "[31:31]", "read-only")
+    ])
 
-    return txt
+    return generate_register(name, "TRNG STAT Register", 0x4, fields)
 
 def generate_register_trng_mode(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>TRNG MODE Register</description>
-                  <addressOffset>0x8</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>r256</name>
-                      <description>256-bit operation mode: 0 - 128-bit mode, 1 - 256-bit mode</description>
-                      <bitRange>[3:3]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(name, "TRNG MODE Register", 0x8, [
+        ("r256", "256-bit operation mode: 0 - 128-bit mode, 1 - 256-bit mode", "[3:3]", "read-write")
+    ])
 
 def generate_register_trng_smode(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>TRNG SMODE Register</description>
-                  <addressOffset>0xc</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>nonce_mode</name>
-                      <description>Nonce operation mode</description>
-                      <bitRange>[2:2]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>mission_mode</name>
-                      <description>Mission operation mode</description>
-                      <bitRange>[8:8]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>max_rejects</name>
-                      <description>TRNG Maximum Rejects</description>
-                      <bitRange>[31:16]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(name, "TRNG SMODE Register", 0xc, [
+        ("nonce_mode", "Nonce operation mode", "[2:2]", "read-write"),
+        ("mission_mode", "Mission operation mode", "[8:8]", "read-write"),
+        ("max_rejects", "TRNG Maximum Rejects", "[31:16]", "read-write")
+    ])
 
 def generate_register_trng_ie(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>TRNG Interrupt Enable Register</description>
-                  <addressOffset>0x10</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>rand_rdy_en</name>
-                      <description>RAND Ready Enable</description>
-                      <bitRange>[0:0]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>seed_done_en</name>
-                      <description>Seed Done Enable</description>
-                      <bitRange>[1:1]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>lfsr_lockup_en</name>
-                      <description>LFSR Lockup Enable</description>
-                      <bitRange>[4:4]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                    <field>
-                      <name>glbl_en</name>
-                      <description>Global Enable</description>
-                      <bitRange>[31:31]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(name, "TRNG Interrupt Enable Register", 0x10, [
+        ("rand_rdy_en", "RAND Ready Enable", "[0:0]", "read-write"),
+        ("seed_done_en", "Seed Done Enable", "[1:1]", "read-write"),
+        ("lfsr_lockup_en", "LFSR Lockup Enable", "[4:4]", "read-write"),
+        ("glbl_en", "Global Enable", "[31:31]", "read-write")
+    ])
 
 def generate_register_trng_istat(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>TRNG Interrupt Status Register</description>
-                  <addressOffset>0x14</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>rand_rdy</name>
-                      <description>RAND Ready Enable</description>
-                      <bitRange>[0:0]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>seed_done</name>
-                      <description>Seed Done Enable</description>
-                      <bitRange>[1:1]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                    <field>
-                      <name>lfsr_lockup_en</name>
-                      <description>LFSR Lockup Enable</description>
-                      <bitRange>[4:4]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(name, "TRNG Interrupt Status Register", 0x14, [
+        ("rand_rdy", "RAND Ready Enable", "[0:0]", "read-only"),
+        ("seed_done", "Seed Done Enable", "[1:1]", "read-only"),
+        ("lfsr_lockup_en", "LFSR Lockup Enable", "[4:4]", "read-only")
+    ])
 
 def generate_register_trng_rand(name, addr, idx):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-
-    desc = "TRNG RAND {} Status Register".format(idx)
-    addr_offset = "0x{:x}".format(addr)
-
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>""" + desc + """</description>
-                  <addressOffset>""" + addr_offset + """</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>rand</name>
-                      <description>Random number bits</description>
-                      <bitRange>[31:0]</bitRange>
-                      <access>read-only</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(name, "TRNG RAND {} Status Register".format(idx), addr, [("rand", "Random number bits", "[31:0]", "read-only")])
 
 def generate_register_trng_auto_rqsts(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>Auto-reseeding after random number requests by host reaches specified counter: 0 - disable counter, other - reload value for internal counter</description>
-                  <addressOffset>0x60</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>rqsts</name>
-                      <description>Threshold number of reseed requests for auto-reseed counter</description>
-                      <bitRange>[31:0]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
+    return generate_register(
+        name,
+        "Auto-reseeding after random number requests by host reaches specified counter: 0 - disable counter, other - reload value for internal counter",
+        0x60,
+        [("rqsts", "Threshold number of reseed requests for auto-reseed counter", "[31:0]", "read-write")],
+    )
 
 def generate_register_trng_auto_age(name):
     """Generate xml string for starfive_jh7110_trng """ + name + """ register"""
-
-    return """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>Auto-reseeding after specified timer countdowns to 0: 0 - disable timer, other - reload value for internal timer</description>
-                  <addressOffset>0x64</addressOffset>
-                  <size>32</size>
-                  <fields>
-                    <field>
-                      <name>age</name>
-                      <description>Countdown value for auto-reseed timer</description>
-                      <bitRange>[31:0]</bitRange>
-                      <access>read-write</access>
-                    </field>
-                  </fields>
-                </register>
-"""
-
-def generate_field_last_reseed(bit, idx):
-    name = "last_reseed{}".format(idx)
-    desc = "TRNG Last Reseed {} status".format(idx)
-    bit_range = "[{}:{}]".format(bit, bit)
-
-    return """\
-                    <field>
-                      <name>""" + name + """</name>
-                      <description>""" + desc + """</description>
-                      <bitRange>""" + bit_range + """</bitRange>
-                      <access>read-only</access>
-                    </field>
-"""
+    return generate_register(
+        name,
+        "Auto-reseeding after specified timer countdowns to 0: 0 - disable timer, other - reload value for internal timer",
+        0x64,
+        [("age", "Countdown value for auto-reseed timer", "[31:0]", "read-write")],
+    )
