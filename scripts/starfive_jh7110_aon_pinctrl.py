abd71f444d53e7824f0cb8f308be1cc54f5a922b
rmsyn <rmsynchls@gmail.com>
2023-12-10 02:01:09 +0000

    fixup: aon_iomux: simplify names, increase code re-use
    Simplify the names from the ones supplied in the JH7110 Technical
    Reference Manual.
    
    Uses the `generate_register` helper function to increase code re-use,
    and add resettable implementations in generated code.

--- scripts/starfive_jh7110_aon_pinctrl.py
+++ scripts/starfive_jh7110_aon_pinctrl.py
@@ -16,172 +16,111 @@ def generate_registers_starfive_jh7110_aon_iomux_cfg(dts, peripheral):
 
     name_desc_range = [
         (
-            "aon_iomux_gpo{}_doen_cfg",
+            "gpo_doen_{}",
             "The selected OEN signal for GPIO{}. The register value indicates the selected GPIO (Output Enable) OEN index from GPIO OEN list 0-5. See Table 2-42: GPIO OEN List for AON_IOMUX for more information.",
             ["[2:0]", "[10:8]", "[18:16]", "[26:24]"]
         ),
         (
-            "aon_iomux_gpo{}_dout_cfg",
+            "gpo_dout_{}",
             "The selected OEN signal for GPIO{}. The register value indicates the selected GPIO output signal list 0-9. See Table 2-42: GPIO OEN List for AON_IOMUX for more information.",
             ["[3:0]", "[11:8]", "[19:16]", "[27:24]"]
         ),
         (
-            "aon_iomux_gpi_u0_pmu_io_event_stub_gpio_wakeup_{}_cfg",
+            "gpi_pmu_wakeup_{}",
             "The register value indicates the selected GPIO number + 2 (GPIO2-GPIO63, GPIO0 and GPIO1 are not available) for the input signal.",
             ["[2:0]", "[10:8]", "[18:16]", "[26:24]"]
         ),
         (
-            "aon_gpioen_{}_reg",
+            "gpen_{}",
             "Enable GPIO IRQ function.",
             ["[0:0]"]
         )
     ]
-    for idx in range(0, len(name_desc_range)):
+    for idx in range(len(name_desc_range)):
         (n, d, br) = name_desc_range[idx]
-        ndra = [(n.format(i), d.format(i), br[i], "read-write") for i in range(0, len(br))]
+        ndra = [(n.format(i), d.format(i), br[i], "read-write") for i in range(len(br))]
         txt += generate_register_aon_iomux_cfgsaif_syscfg_fmux(idx, ndra)
 
     name_desc_access = [
-        ("aon_gpiois_0_reg", "1: Edge trigger, 0: Level trigger", "read-write"),
-        ("aon_gpioic_0_reg", "1: Do not clear the register, 0: Clear the register", "read-write"),
-        ("aon_gpioibe_0_reg", "1: Trigger on both edges, 0: Trigger on a single edge", "read-write"),
-        ("aon_gpioiev_0_reg", "1: Positive/Low, 0: Negative/High", "read-write"),
-        ("aon_gpioie_0_reg", "1: Unmask, 0: Mask", "read-write"),
-        ("aon_gpioris_0_reg", "Status of the edge trigger, can be cleared by writing gpioic.", "read-only"),
-        ("aon_gpiomis_0_reg", "The masked GPIO IRQ status.", "read-only"),
-        ("aon_gpio_in_sync2_0_reg", "Status of gpio_in after synchronization.", "read-only")
+        ("is", "1: Edge trigger, 0: Level trigger", "read-write"),
+        ("ic", "1: Do not clear the register, 0: Clear the register", "read-write"),
+        ("ibe", "1: Trigger on both edges, 0: Trigger on a single edge", "read-write"),
+        ("iev", "1: Positive/Low, 0: Negative/High", "read-write"),
+        ("ie", "1: Unmask, 0: Mask", "read-write"),
+        ("ris", "Status of the edge trigger, can be cleared by writing gpioic.", "read-only"),
+        ("mis", "The masked GPIO IRQ status.", "read-only"),
+        ("in_sync2", "Status of gpio_in after synchronization.", "read-only")
     ]
-    for idx in range(4, 12):
-        txt += generate_register_aon_iomux_cfgsaif_syscfg_ioirq(idx, name_desc_access[idx - 4])
+    for idx in range(len(name_desc_access)):
+        txt += generate_register_aon_iomux_cfgsaif_syscfg_ioirq(idx, 16 + (idx * 4), name_desc_access[idx])
 
 
-    name_desc_range_access = [("padcfg_pad_testen_pos", "Power-on-Start (POS) enabler - 1: Enable active pull down for loss of core power, 0: Active pull-down capability disabled", "[0:0]", "read-write")]
-    txt += generate_register_aon_iomux_cfgsaif_syscfg(48, name_desc_range_access)
+    txt += generate_register("testen", "AON IOMUX CFG SAIF SYSCFG 48", 48, [
+        (
+            "testen_pos",
+            "Power-on-Start (POS) enabler - 1: Enable active pull down for loss of core power, 0: Active pull-down capability disabled",
+            "[0:0]",
+            "read-write"
+        )
+    ])
 
+    for idx in range(4): 
+        txt += generate_register_aon_iomux_cfgsaif_syscfg_padcfg(idx, 52 + (idx * 4))
 
-    for idx in range(0, 4): 
-        txt += generate_register_aon_iomux_cfgsaif_syscfg_padcfg(52 + (idx * 4), "rgpio{}".format(idx))
-
-    name_desc_range_access = [
-        ("padcfg_pad_rstn_smt", "Active high Schmitt (SMT) trigger selector - 0: No hysteresis, 1: Schmitt trigger enabled", "[0:0]", "read-write"),
-        ("padcfg_pad_rstn_pos", "Power-on-Start (POS) enabler - 1: Enable active pull-down for loss of core power, 0: Active pull-down capability disabled", "[1:1]", "read-write"),
-    ]
-    txt += generate_register_aon_iomux_cfgsaif_syscfg(68, name_desc_range_access)
+    txt += generate_register("rstn", "AON IOMUX CFG SAIF SYSCFG 68", 68, [
+        ("smt", "Active high Schmitt (SMT) trigger selector - 0: No hysteresis, 1: Schmitt trigger enabled", "[0:0]", "read-write"),
+        ("pos", "Power-on-Start (POS) enabler - 1: Enable active pull-down for loss of core power, 0: Active pull-down capability disabled", "[1:1]", "read-write"),
+    ])
 
     desc = "Output Drive Strength (DS): * 00: The rated drive strength is 2 mA. * 01: The rated drive strength is 4 mA. * 10: The rated drive strength is 8 mA. * 11: The rated drive strength is 12 mA."
 
-    name_desc_range_access = [
-        [("padcfg_pad_rtc_ds", desc, "[1:0]", "read-write")],
-        [("padcfg_pad_osc_ds", desc, "[1:0]", "read-write")]
-    ]
-    for idx in range(0, len(name_desc_range_access)):
-        txt += generate_register_aon_iomux_cfgsaif_syscfg(76 + (idx * 8), name_desc_range_access[idx])
+    txt += generate_register("rtc", "AON IOMUX CFG SAIF SYSCFG 76", 76, [("ds", desc, "[1:0]", "read-write")])
+    txt += generate_register("osc", "AON IOMUX CFG SAIF SYSCFG 84", 84, [("ds", desc, "[1:0]", "read-write")])
 
     name_desc_range_access = [
-        [("padcfg_pad_gmac0_mdc_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_mdio_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxd0_syscon", "0: GMAC0 IO voltage select 3.3V, 1: GMAC0 IO voltage select 2.5V, 2: GMAC0 IO voltage select 1.8V", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxd1_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxd2_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxd3_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxdv_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_rxc_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txd0_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txd1_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txd2_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txd3_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txen_syscon", "", "[1:0]", "read-write")],
-        [("padcfg_pad_gmac0_txc_syscon", "", "[1:0]", "read-write")],
-        [("pad_gmac0_rxc_func_sel", "Function selector of GMAC0_RXC: * Function 0: u0_aon_crg_clk_gmac0_rgmii_rx, * Function 1: u0_aon_crg_clk_gmac0_rmii_ref, * Function 2: None, * Function 3: None", "[1:0]", "read-write")]
+        ("gmac0_mdc", "", "[1:0]", "read-write"),
+        ("gmac0_mdio", "", "[1:0]", "read-write"),
+        ("gmac0_rxd0", "0: GMAC0 IO voltage select 3.3V, 1: GMAC0 IO voltage select 2.5V, 2: GMAC0 IO voltage select 1.8V", "[1:0]", "read-write"),
+        ("gmac0_rxd1", "", "[1:0]", "read-write"),
+        ("gmac0_rxd2", "", "[1:0]", "read-write"),
+        ("gmac0_rxd3", "", "[1:0]", "read-write"),
+        ("gmac0_rxdv", "", "[1:0]", "read-write"),
+        ("gmac0_rxc", "", "[1:0]", "read-write"),
+        ("gmac0_txd0", "", "[1:0]", "read-write"),
+        ("gmac0_txd1", "", "[1:0]", "read-write"),
+        ("gmac0_txd2", "", "[1:0]", "read-write"),
+        ("gmac0_txd3", "", "[1:0]", "read-write"),
+        ("gmac0_txen", "", "[1:0]", "read-write"),
+        ("gmac0_txc", "", "[1:0]", "read-write"),
+        ("gmac0_rxc_func_sel", "Function selector of GMAC0_RXC: * Function 0: u0_aon_crg_clk_gmac0_rgmii_rx, * Function 1: u0_aon_crg_clk_gmac0_rmii_ref, * Function 2: None, * Function 3: None", "[1:0]", "read-write")
     ]
-    for idx in range(0, len(name_desc_range_access)):
-        txt += generate_register_aon_iomux_cfgsaif_syscfg(88 + (idx * 4), name_desc_range_access[idx])
+    for idx in range(len(name_desc_range_access)):
+        (name, desc, bit_range, access) = name_desc_range_access[idx]
+        addr = 88 + (idx * 4)
+
+        txt += generate_register(name, "AON IOMUX CFG SAIF SYSCFG {}".format(addr), addr, [("value", desc, bit_range, access)]) 
 
     return txt + """\
               </registers>
 """
 
 def generate_register_aon_iomux_cfgsaif_syscfg_fmux(idx, name_desc_range_access):
-    name = "aon_iomux_cfgsaif_syscfg_fmux{}".format(idx)
-    desc = "AON IOMUX CFG SAIF SYSCFG FMUX {}".format(idx)
-    addr = "0x{:x}".format(idx * 4)
+    name = "fmux_{}".format(idx)
+    addr = idx * 4
+    desc = "AON IOMUX CFG SAIF SYSCFG FMUX {}".format(addr)
 
-    txt = """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>""" + desc + """</description>
-                  <addressOffset>""" + addr + """</addressOffset>
-                  <size>32</size>
-                  <fields>
-"""
+    return generate_register(name, desc, addr, name_desc_range_access) 
 
-    for i in range(0, len(name_desc_range_access)):
-        (name, desc, bit_range, access) = name_desc_range_access[i]
-        txt += generate_field(name, desc, bit_range, access)
+def generate_register_aon_iomux_cfgsaif_syscfg_ioirq(idx, addr, name_desc_access):
+    name = "ioirq_{}".format(idx)
+    desc = "AON IOMUX CFG SAIF SYSCFG IOIRQ {}".format(addr)
+    (n, d, a) = name_desc_access
 
-    return txt + """\
-                  </fields>
-                </register>
-"""
+    return generate_register(name, desc, addr, [(n, d, "[3:0]", a)])
 
-def generate_register_aon_iomux_cfgsaif_syscfg_ioirq(idx, name_desc_access):
-    name = "aon_iomux_cfgsaif_syscfg_ioirq{}".format(idx)
-    desc = "AON IOMUX CFG SAIF SYSCFG IOIRQ {}".format(idx)
-    addr = "0x{:x}".format(idx * 4)
-
-    txt = """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>""" + desc + """</description>
-                  <addressOffset>""" + addr + """</addressOffset>
-                  <size>32</size>
-                  <fields>
-"""
-
-    (name, desc, access) = name_desc_access
-    txt += generate_field(name, desc, "[3:0]", access)
-
-    return txt + """\
-                  </fields>
-                </register>
-"""
-
-def generate_register_aon_iomux_cfgsaif_syscfg(idx, name_desc_range_access):
-    name = "aon_iomux_cfgsaif_syscfg{}".format(idx)
-    desc = "AON IOMUX CFG SAIF SYSCFG {}".format(idx)
-    addr = "0x{:x}".format(idx)
-
-    txt = """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>""" + desc + """</description>
-                  <addressOffset>""" + addr + """</addressOffset>
-                  <size>32</size>
-                  <fields>
-"""
-
-    for i in range(0, len(name_desc_range_access)):
-        (name, desc, bit_range,  access) = name_desc_range_access[i]
-        txt += generate_field(name, desc, bit_range, access)
-
-    return txt + """\
-                  </fields>
-                </register>
-"""
-
-def generate_register_aon_iomux_cfgsaif_syscfg_padcfg(idx, pin):
-    name = "aon_iomux_cfgsaif_syscfg{}".format(idx)
-    desc = "AON IOMUX CFG SAIF SYSCFG {}".format(idx)
-    addr = "0x{:x}".format(idx)
-
-    txt = """\
-                <register>
-                  <name>""" + name + """</name>
-                  <description>""" + desc + """</description>
-                  <addressOffset>""" + addr + """</addressOffset>
-                  <size>32</size>
-                  <fields>
-"""
+def generate_register_aon_iomux_cfgsaif_syscfg_padcfg(idx, addr):
+    name = "rgpio_{}".format(idx)
+    desc = "AON IOMUX CFG SAIF SYSCFG {}".format(addr)
 
     name_desc_range = [
             ("ie", "Input Enable (IE) Controller - 1: Enable the receiver, 0: Disable the receiver", "[0:0]"),
@@ -192,12 +131,6 @@ def generate_register_aon_iomux_cfgsaif_syscfg_padcfg(idx, pin):
             ("smt", "Active high Schmitt (SMT) trigger selector - 0: No hysteresis, 1: Schmitt trigger ebabled", "[6:6]"),
             ("pos", "Power-on-Start (POS) enabler - 1: Enable active pull down for loss of core power, 0: Active pull-down capability disabled", "[7:7]"),
     ]
+    fields = [(n, d, r, "read-write") for (n, d, r) in name_desc_range]
 
-    for idx in range(0, len(name_desc_range)):
-        (name, desc, bit_range) = name_desc_range[idx]
-        txt += generate_field("padcfg_pad_{}_{}".format(pin, name), desc, bit_range, "read-write")
-
-    return txt + """\
-                  </fields>
-                </register>
-"""
+    return generate_register(name, desc, addr, fields)
